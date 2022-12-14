760331131c4943929c6ba8ca1dc940f60e78623a
Sonnm <kienthanh2seri@gmail.com>
2022-12-14 23:21:53 +0700

    May tinh Casio Simple

---
+++ training-react/src/components/CalTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CalTitle = () => {
+    return (
+        <div className="calculator-title ">
+            Máy tính Casio cầm tay siêu cấp VIP
+        </div>
+    )
+}
+
+export default CalTitle;
